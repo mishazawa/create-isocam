94a1626a07fd1178f52ea986236cd6b2b43dde6d
unknown <macintoshkrug@gmail.com>
2021-09-16 21:48:31 +0300

    create submenu in 'add' submenu (Shift + a)
    edit readme

--- createisocam.py
+++ createisocam.py
@@ -1,10 +1,16 @@
 ﻿# This script creates two kinds of isometric cameras.
-#The one, TrueIsocam called camera, is the mathematical correct isometric camera with the 54.736 rotation to get the 30 degrees angles at the sides of the rhombus.
-#The other, GameIsocam called camera, is a camera with which you can render isometric tiles for a 2d game. Here we need a 60 degrees angle instedad of the 54.736 one to get a proper stairs effect and a ratio of 2:1
-# Then there is the special case with a 4:3 ratio, which is button 3. You can also make 2D games with that one. The view is more topdown though as with a 2:1 ratio of the traditional game iso view.
-# The fourth button creates a simple groundplane where you can place your stuff at.
+# The one, TrueIsocam called camera, is the mathematical correct isometric camera
+# with the 54.736 rotation to get the 30 degrees angles at the sides of the rhombus.
 
-#You can of course set up everything by hand. This script is a convenient solution so that you don't have to setup it again and again.
+# The other, GameIsocam called camera, is a camera with which you can render
+# isometric tiles for a 2d game. Here we need a 60 degrees angle instedad of
+# the 54.736 one to get a proper stairs effect and a ratio of 2:1
+
+# Then there is the special case with a 4:3 ratio, which is button 3.
+# You can also make 2D games with that one. The view is more topdown
+# though as with a 2:1 ratio of the traditional game iso view.
+
+# You can of course set up everything by hand. This script is a convenient solution so that you don't have to setup it again and again.
 
 # The script is under Apache license
 
@@ -19,131 +25,95 @@ bl_info = {
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/Scripts/Add_Mesh/Create_IsoCam",
     "category": "Create"}
 
-
 import bpy
+from bpy.types import Operator
+from bpy_extras.object_utils import AddObjectHelper
 
-# ----------------------------------------- true isometric camera
-class PR_OT_createtrueisocam(bpy.types.Operator):
-    """Creates a camera for mathematical correct isometric view"""
+ORTHO_SCALE = 14.123
+CAMERA_TYPE = 'ORTHO'
+BLENDER_OPT = {'REGISTER', 'UNDO'}
+
+def setup_camera(object, rotation, bl_label):
+    object.rotation_euler = rotation
+    object.data.type = CAMERA_TYPE
+    object.data.ortho_scale = ORTHO_SCALE
+    object.name = bl_label
+
+class TrueIsoCam(Operator, AddObjectHelper):
+    """Create a new TrueIsocam"""
     bl_idname = "scene.create_trueisocam"
     bl_label = "TrueIsocam"
-    bl_options = {'REGISTER', 'UNDO'}
+    bl_options = BLENDER_OPT
 
     def execute(self, context):
-
-        # ----------------------------Create Camera with correct position and rotation
-        bpy.ops.object.camera_add(location=(30.60861, -30.60861, 30.60861)) # Create Camera. I would love to set the rotation here too. Blender not. Not that there are no tutorials around which shows that it should work ... .
-
-        #So that's what the next two lines are good for. Setting the rotation of the camera ...
-
-        object = bpy.context.active_object
-        object.rotation_euler = (0.955324, 0, 0.785398) #Attention, these are radians. Euler angles are (54.736,0,45) Here we set the rotation for a mathematical correct isometric view. Not to mix with the Isoview for a 2D game!
-
-        # ------------------------------Here we adjust some settings ---------------------------------
-        object.data.type = 'ORTHO' # We want Iso, so set the type of the camera to orthographic
-        object.data.ortho_scale = 14.123 # Let's fit the camera to a basetile in size of 10
-        object.name = "TrueIsoCam" # let's rename the cam so that it cannot be confused with other cameras.
-        bpy.ops.view3d.object_as_camera() # Set the current camera as the active one to look through
+        bpy.ops.object.camera_add(location=(30.60861, -30.60861, 30.60861))
+        setup_camera(bpy.context.active_object, (0.955324, 0, 0.785398), self.bl_label)
+        bpy.ops.view3d.object_as_camera()
 
         return {'FINISHED'}
-# ----------------------------------------- Game isometric camera
-class PR_OT_creategameisocam(bpy.types.Operator):
+
+class GameIsoCam(bpy.types.Operator):
     """Creates a camera with isometric view for game needs"""
     bl_idname = "scene.create_gameisocam"
     bl_label = "GameIsocam"
-    bl_options = {'REGISTER', 'UNDO'}
+    bl_options = BLENDER_OPT
 
     def execute(self, context):
-
-        # ----------------------------Create Camera with correct position and rotation
-        bpy.ops.object.camera_add(location=(30.60861, -30.60861, 25.00000)) # Create Camera. I would love to set the rotation here too. Blender not. Not that there are no tutorials around which shows that it should work ... .
-
-        #So that's what the next two lines are good for. Setting the rotation of the camera ...
-
-        object = bpy.context.active_object
-        object.rotation_euler = (1.047198, 0, 0.785398)#Attention, these are radians. Euler angles are (60,0,45) Here we set the rotation for a isometric view that is used in 2D games. Not to mix with the mathematical correct Isoview!
-
-        # ------------------------------Here we adjust some settings ---------------------------------
-        object.data.type = 'ORTHO' # We want Iso, so set the type of the camera to orthographic
-        object.data.ortho_scale = 14.123  # Let's fit the camera to our basetile in size of 10
-        object.name = "GameIsoCam" # let's rename the cam so that it cannot be confused with other cameras.
-        bpy.ops.view3d.object_as_camera() # Set the current camera as the active one to look through
-
+        bpy.ops.object.camera_add(location=(30.60861, -30.60861, 25.00000))
+        setup_camera(bpy.context.active_object, (1.047198, 0, 0.785398), self.bl_label)
+        bpy.ops.view3d.object_as_camera()
 
         return {'FINISHED'}
 
-# ----------------------------------------- Game isometric camera 4 to 3
-# This format is not so common. But is also used here and there. It is more topdown. With a basetile of 64 to 48 pixels
-
-class PR_OT_creategameisocam4to3(bpy.types.Operator):
+class GameIsoCam4x3(bpy.types.Operator):
     """Creates a camera with a special 4:3 iso view for game needs"""
     bl_idname = "scene.create_gameisocam4to3"
     bl_label = "GameIsocam4to3"
-    bl_options = {'REGISTER', 'UNDO'}
+    bl_options = BLENDER_OPT
 
     def execute(self, context):
-
-        # ----------------------------Create Camera with correct position and rotation
-        bpy.ops.object.camera_add(location=(23.42714, -23.42714, 37.4478)) # Create Camera. I would love to set the rotation here too. Blender not. Not that there are no tutorials around which shows that it should work ... .
-
-        #So that's what the next two lines are good for. Setting the rotation of the camera ...
-
-        object = bpy.context.active_object
-        object.rotation_euler = (0.724312, 0, 0.785398)#Attention, these are radians. Euler angles are (41.5,0,45) Here we set the rotation for a isometric view that is used in 2D games. Not to mix with the mathematical correct Isoview!
-
-        # ------------------------------Here we adjust some settings ---------------------------------
-        object.data.type = 'ORTHO' # We want Iso, so set the type of the camera to orthographic
-        object.data.ortho_scale = 14.123  # Let's fit the camera to our basetile in size of 10
-        object.name = "GameIso4to3Cam" # let's rename the cam so that it cannot be confused with other cameras.
-        bpy.ops.view3d.object_as_camera() # Set the current camera as the active one to look through
-
+        bpy.ops.object.camera_add(location=(23.42714, -23.42714, 37.4478))
+        setup_camera(bpy.context.active_object, (0.724312, 0, 0.785398), self.bl_label)
+        bpy.ops.view3d.object_as_camera()
 
         return {'FINISHED'}
 
-# ----------------------------------------- Create a ground plane
-
-class PR_OT_creategroundplane(bpy.types.Operator):
-    """Creates a groundplane in size of ten where you can put your things on"""
-    bl_idname = "scene.create_groundplane"
-    bl_label = "Groundplane"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    def execute(self, context):
-        bpy.ops.mesh.primitive_plane_add(location=(0, 0, 0)) # Create Camera. I would love to set the scale here too. Blender not. So let's do it in an extra step
-
-        object = bpy.context.active_object
-        object.scale = (5, 5, 0)#The plane object is created with a size of 2. Scaling it to 10 means to scale it by factor 5
-        bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)# apply scale
-
-        return {'FINISHED'}
-
-#----------------------------------------- Create panel in the toolshelf -------------------------------------------------
-
-class PR_PT_createisocampanel(bpy.types.Panel):
-    bl_label = "Create IsoCam"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-    bl_category = "PR Isocam"
+class IsoCamMenu(bpy.types.Menu):
+    bl_idname = "OBJECT_MT_select_submenu"
+    bl_label = "Select"
 
     def draw(self, context):
-
-        # column buttons solution. Less space than single buttons ...
         layout = self.layout
-        view = context.space_data
-        # Three buttons
-        col = layout.column(align=True)
-        col.operator("scene.create_trueisocam", text="TrueIsocam")
-        col.operator("scene.create_gameisocam", text="GameIsocam")
-        col.operator("scene.create_gameisocam4to3", text="GameIso4to3cam")
-        col.operator("scene.create_groundplane", text="Groundplane")
-# -------------------------------------------------------------------------------------------
 
-# store keymaps here to access after registration
-addon_keymaps = []
+        layout.operator("scene.create_trueisocam", text="Isometric Camera", icon='VIEW_CAMERA')
+        layout.operator("scene.create_gameisocam", text="Isometric Game Camera", icon='VIEW_CAMERA')
+        layout.operator("scene.create_gameisocam4to3", text="Isometric Game Camera 4:3", icon='VIEW_CAMERA')
 
-classes = (PR_OT_createtrueisocam, PR_OT_creategameisocam, PR_OT_creategameisocam4to3, PR_OT_creategroundplane, PR_PT_createisocampanel)
+# Registration
 
-register, unregister = bpy.utils.register_classes_factory(classes)
+def add_Menu_button(self, context):
+    self.layout.separator()
+    self.layout.menu(
+        IsoCamMenu.bl_idname,
+        text="Isometric Cameras",
+        icon='VIEW_CAMERA')
+
+
+def register():
+    bpy.utils.register_class(TrueIsoCam)
+    bpy.utils.register_class(GameIsoCam)
+    bpy.utils.register_class(GameIsoCam4x3)
+    bpy.utils.register_class(IsoCamMenu)
+    bpy.types.VIEW3D_MT_add.append(add_Menu_button)
+
+
+def unregister():
+    bpy.utils.unregister_class(TrueIsoCam)
+    bpy.utils.unregister_class(GameIsoCam)
+    bpy.utils.unregister_class(GameIsoCam4x3)
+    bpy.utils.unregister_class(IsoCamMenu)
+    bpy.types.VIEW3D_MT_add.remove(add_Menu_button)
+
 
 if __name__ == "__main__":
     register()
